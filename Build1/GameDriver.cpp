6d72ce3290be8d1287a9e1111a173e7d9581e5b1
ocabana <olyver.cabana@live.com>
2016-03-08 17:37:06 -0500

    Added files via upload

--- Build1/GameDriver.cpp
+++ Build1/GameDriver.cpp
@@ -24,7 +24,18 @@ void GameDriver::initGame(){
 void GameDriver::playTurn(){
 	
 	//Determine player order
+	//Temp vector of players
+	std::vector<Player> playersInOrder;
+	//If the first player has more houses
+	if (players[0].getNumberOfHouses() < players[1].getNumberOfHouses()){
 
+	}//if the second player has more houses
+	else if (players[0].getNumberOfHouses() > players[1].getNumberOfHouses()){
+
+	}//if they have the same amount of houses
+	else{
+
+	}
 	//Auction PowerPlants
 
 	//Buy Resources
